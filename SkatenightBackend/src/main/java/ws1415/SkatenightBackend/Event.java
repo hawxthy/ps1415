377465d8cbcc7249d4697a3043bec6177e59e7d5
Bernd Eissing <b_eiss02@uni-muenster.de>
2014-10-21 13:46:02 +0200

    Event Klasse geändert, um Attribute

--- SkatenightBackend/src/main/java/ws1415/SkatenightBackend/Event.java
+++ SkatenightBackend/src/main/java/ws1415/SkatenightBackend/Event.java
@@ -1,16 +1,52 @@
 package ws1415.SkatenightBackend;
 
 /**
- * Created by Richard on 21.10.2014.
+ * Created by Bernd Eissing, Martin Wrodarczyk on 21.10.2014.
  */
 public class Event {
-    private String name;
+    private String title;
+    private String date;
+    private String fee;
+    private String location;
+    private String description;
 
-    public String getName() {
-        return name;
+    public String getTitle() {
+        return title;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getFee() {
+        return fee;
+    }
+
+    public void setFee(String fee) {
+        this.fee = fee;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
