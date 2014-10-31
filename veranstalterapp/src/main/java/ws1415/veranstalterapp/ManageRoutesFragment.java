43ca58a749329104915ee0d0d4e0f00054299427
Bernd Eissing <b_eiss02@uni-muenster.de>
2014-10-31 13:14:30 +0100

    ListView Adapter für die Routen erstellt.

--- veranstalterapp/src/main/java/ws1415/veranstalterapp/ManageRoutesFragment.java
+++ veranstalterapp/src/main/java/ws1415/veranstalterapp/ManageRoutesFragment.java
@@ -6,18 +6,55 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.SimpleCursorAdapter;
+
+import ws1415.veranstalterapp.Adaper.MapsCursorAdapter;
 
 /**
  *
  */
 public class ManageRoutesFragment extends Fragment {
+    private ListView routeList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_manage_routes, container, false);
+
+        routeList = (ListView) view.findViewById(R.id.manage_routes_route_list);
+        routeList.setAdapter(new MapsCursorAdapter(getActivity(), getRoutes()));
+        routeList.setOnItemClickListener( new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+
         return view;
+    }
+
+
+
+    private ArrayList<Route> getRoutes(){
+        List<Route> tmpList = new ArrayList<Route>();
+        // Erstelle test Routes
+        Route route1 = new Route();
+        Route route2 = new Route();
+        Route route3 = new Route();
+        route1.setName("Route 1");
+        route2.setName("Route 2");
+        route3.setName("Route 3");
+        route1.setLength(10);
+        route2.setLength(11);
+        route3.setLength(12);
+
+        tmpList.add(route1);
+        tmpList.add(route2);
+        tmpList.add(route3);
+
+        return tmpList;
     }
 
     //private void populateListView(){
