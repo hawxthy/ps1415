1471b9a1658f773bb2cbb3f7c9184d5850593150
RichardSchulze <richard-schulze@online.de>
2014-11-11 14:52:21 +0100

    URL für den Testserver angepasst

--- veranstalterapp/src/main/java/ws1415/veranstalterapp/ServiceProvider.java
+++ veranstalterapp/src/main/java/ws1415/veranstalterapp/ServiceProvider.java
@@ -58,7 +58,7 @@ public abstract class ServiceProvider {
         SkatenightAPI.Builder builder = new SkatenightAPI.Builder(
                 AndroidHttp.newCompatibleTransport(),
                 new AndroidJsonFactory(), null);
-        builder.setRootUrl("http://192.168.178.21:8080/_ah/api");
+        builder.setRootUrl("http://10.66.20.74:8080/_ah/api");
         service = builder.build();
     }
 
