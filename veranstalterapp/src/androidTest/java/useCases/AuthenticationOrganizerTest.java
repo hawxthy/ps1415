74baece81c0068118930b650ff46235eccbd0342
t_rust01 <tristan.rust@gmx.de>
2014-11-25 16:51:31 +0100

    AuthenticationOrganizerTest geupdated

--- veranstalterapp/src/androidTest/java/useCases/AuthenticationOrganizerTest.java
+++ veranstalterapp/src/androidTest/java/useCases/AuthenticationOrganizerTest.java
@@ -1,9 +1,57 @@
 package useCases;
 
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+
+import ws1415.veranstalterapp.Constants;
+import ws1415.veranstalterapp.ServiceProvider;
+import ws1415.veranstalterapp.activity.LoginActivity;
+
 /**
- * Testet den Use Case "Aktuelle Informationen anzeigen".
+ * Testet den Use Case "Authentifikation des Veranstalters".
  *
  * @author Tristan Rust
  */
-public class AuthenticationOrganizerTest {
+public class AuthenticationOrganizerTest extends ActivityInstrumentationTestCase2<LoginActivity> {
+
+    private LoginActivity mActivity;
+
+    public AuthenticationOrganizerTest() {
+        super(LoginActivity.class);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        // Nutzer einloggen
+        GoogleAccountCredential credential = GoogleAccountCredential.usingAudience(
+                getActivity(),
+                "server:client_id:"+ Constants.WEB_CLIENT_ID);
+        credential.setSelectedAccountName(credential.getAllAccounts()[0].name);
+        ServiceProvider.login(credential);
+
+        setActivityInitialTouchMode(false);
+
+        // Holt sich die LoginActiviy und startet diese
+        mActivity = getActivity();
+
+    }
+
+    public void testPreConditions() {
+
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+
