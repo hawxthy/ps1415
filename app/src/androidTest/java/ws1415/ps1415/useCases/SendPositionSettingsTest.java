b8c83aa61e61e702f88f4d98db2a519525db3306
t_rust01 <tristan.rust@gmx.de>
2015-01-06 16:14:44 +0100

    Test für die Setting hinzugefügt

---
+++ app/src/androidTest/java/ws1415/ps1415/useCases/SendPositionSettingsTest.java
@@ -0,0 +1,85 @@
+package ws1415.ps1415.useCases;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.skatenight.skatenightAPI.model.Field;
+import com.skatenight.skatenightAPI.model.Route;
+
+import ws1415.ps1415.Constants;
+import ws1415.ps1415.R;
+import ws1415.ps1415.ServiceProvider;
+import ws1415.ps1415.activity.ShowEventsActivity;
+import com.skatenight.skatenightAPI.model.Event;
+
+import java.util.ArrayList;
+
+import ws1415.ps1415.activity.ShowInformationActivity;
+import ws1415.ps1415.util.EventUtils;
+
+/**
+ *
+ *
+ * @author Tristan Rust
+ */
+public class SendPositionSettingsTest extends ActivityInstrumentationTestCase2<ShowEventsActivity> {
+
+    private ShowEventsActivity mActivity;
+
+    // ShowEventsActivty UI Elemente
+    private ListView mList;
+    private ListAdapter mListData;
+
+    private Event testEvent;
+    private Route testRoute;
+
+    public SendPositionSettingsTest() {
+        super(ShowEventsActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        // Nutzer einloggen
+        GoogleAccountCredential credential = GoogleAccountCredential.usingAudience(getActivity(), "server:client_id:"+ Constants.WEB_CLIENT_ID);
+        credential.setSelectedAccountName(credential.getAllAccounts()[0].name);
+        ServiceProvider.login(credential);
+
+        // Touchmode ausschalten, damit auch die UI Elemente getestet werden können
+        setActivityInitialTouchMode(false);
+
+        // Die ShowEventsActivity starten
+        mActivity = getActivity();
+        // Holt sich die Event Listen-Elemente
+        mList = (ListView) mActivity.findViewById(R.id.activity_show_events_list_view);
+        mListData = mList.getAdapter();
+
+    }
+
+    /**
+     * Prüfen, ob Events in der Liste vorhanden sind.
+     */
+    public void testPreConditions() {
+        assertNotNull("Selection listener on events list initialized", mList.getOnItemClickListener());
+        assertNotNull("adapter for events initialized", mListData);
+
+        // Mindestens ein Event muss exisitieren
+        assertTrue("at least on event exists", mListData.getCount() > 0);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
