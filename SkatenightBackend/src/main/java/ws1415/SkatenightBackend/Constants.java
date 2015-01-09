310d64e39a78872c8b5846428f9a29e1f455e693
RichardSchulze <richard-schulze@online.de>
2015-01-09 11:14:10 +0100

    Productionserver-Config übernommen

--- SkatenightBackend/src/main/java/ws1415/SkatenightBackend/Constants.java
+++ SkatenightBackend/src/main/java/ws1415/SkatenightBackend/Constants.java
@@ -7,15 +7,15 @@ public class Constants {
     /**
      * Client-ID für die normale App.
      */
-    public static final String ANDROID_USER_CLIENT_ID = "644721617929-mk0do3jm5lec1dasijdj3220ot87gbn7.apps.googleusercontent.com";
+    public static final String ANDROID_USER_CLIENT_ID = "37947570052-g006o3ovfotnjqreltom6c7cbktm7dap.apps.googleusercontent.com";
     /**
      * Client-ID für die Veranstalter-App.
      */
-    public static final String ANDROID_HOST_CLIENT_ID = "644721617929-kbcha00vb3j30sh9at05sagm73iltqqo.apps.googleusercontent.com";
+    public static final String ANDROID_HOST_CLIENT_ID = "37947570052-4ru7asfhnrjmmqvj3qdpo2rrp31fudf3.apps.googleusercontent.com";
     /**
      * Client-ID für Webaufrufe.
      */
-    public static final String WEB_CLIENT_ID = "644721617929-unb9em0kl73b9evdv2h52ufn26fao20p.apps.googleusercontent.com";
+    public static final String WEB_CLIENT_ID = "37947570052-dk3rjhgran1s38gscv6va2rmmv2bei8r.apps.googleusercontent.com";
 
     /**
      * Definiert die Webclient-ID als Android Audience, da aus den Apps der Zugriff auf das Back-
