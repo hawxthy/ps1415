a368f94aec0e811981ff9ba986cda1d73921d512
Pascal Otto <p_otto03@uni-muenster.de>
2015-01-16 13:34:37 +0100

    Algorithmus zur Darstellung des Geschwindigkeitsprofils implementiert

--- app/src/main/java/ws1415/ps1415/activity/ShowRouteActivity.java
+++ app/src/main/java/ws1415/ps1415/activity/ShowRouteActivity.java
@@ -4,13 +4,13 @@ import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -25,6 +25,7 @@ import com.google.android.gms.maps.model.PolylineOptions;
 import com.skatenight.skatenightAPI.model.Member;
 
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
 import ws1415.common.util.LocationUtils;
@@ -42,6 +43,7 @@ public class ShowRouteActivity extends Activity {
     public static final String EXTRA_EVENT_ID = "show_route_extra_event_id";
     private static final String MEMBER_ROUTE = "show_route_member_route";
     private static final String MEMBER_ROUTE_HIGHLIGHT = "show_route_member_route_highlight";
+    private static final String MEMBER_ROUTE_TRACK = "show_route_member_route_track";
     private static final String MEMBER_ROUTE_FIELD_FIRST = "show_route_member_route_field_first";
     private static final String MEMBER_ROUTE_FIELD_LAST = "show_route_member_route_field_last";
     private static final String MEMBER_EVENT_ID = "show_route_member_event_id";
@@ -51,8 +53,12 @@ public class ShowRouteActivity extends Activity {
     private Polyline routeLine;
     private PolylineOptions routeHighlight;
     private Polyline routeHighlightLine;
+    private List<PolylineOptions> routeTrack;
+    private List<Polyline> routeTrackLines;
     private int fieldFirst;
     private int fieldLast;
+    private long eventId;
+    private LocationReceiver receiver;
 
     private Location location; // Enthält die aktuelle Position, die vom Server runtergeladen wurde
 
@@ -63,6 +69,9 @@ public class ShowRouteActivity extends Activity {
 
         googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.show_route_map_fragment)).getMap();
         googleMap.setMyLocationEnabled(true);
+
+        routeTrack = new ArrayList<PolylineOptions>();
+        routeTrackLines = new ArrayList<Polyline>();
 
         Intent intent;
         if (savedInstanceState != null) {
@@ -79,6 +88,17 @@ public class ShowRouteActivity extends Activity {
             }
             if (savedInstanceState.containsKey(MEMBER_ROUTE_FIELD_LAST)) {
                 fieldLast = savedInstanceState.getInt(MEMBER_ROUTE_FIELD_LAST);
+            }
+            if (savedInstanceState.containsKey(MEMBER_EVENT_ID)) {
+                eventId = savedInstanceState.getInt(MEMBER_EVENT_ID);
+
+                if (eventId > 0) {
+                    receiver = new LocationReceiver();
+                }
+            }
+            if (savedInstanceState.containsKey(MEMBER_ROUTE_TRACK)) {
+                routeTrack = savedInstanceState.getParcelableArrayList(MEMBER_ROUTE_TRACK);
+                drawTrack();
             }
         }
         else if ((intent = getIntent()) != null && intent.hasExtra(EXTRA_ROUTE)) {
@@ -114,11 +134,18 @@ public class ShowRouteActivity extends Activity {
                 if (intent.hasExtra(EXTRA_ROUTE_FIELD_LAST)) {
                     fieldLast = intent.getIntExtra(EXTRA_ROUTE_FIELD_LAST, 0);
                 }
+
                 Toast.makeText(getApplicationContext(), fieldFirst + " " + fieldLast, Toast.LENGTH_LONG).show();
             }
             catch (ParseException e) {
                 Toast.makeText(getApplicationContext(), "Route parsing failed.", Toast.LENGTH_SHORT).show();
                 e.printStackTrace();
+            }
+
+            eventId = intent.getLongExtra(EXTRA_EVENT_ID, -1);
+
+            if (eventId > 0) {
+                receiver = new LocationReceiver();
             }
         }
 
@@ -131,10 +158,16 @@ public class ShowRouteActivity extends Activity {
     @Override
     protected void onStart() {
         super.onStart();
+        if (receiver != null) {
+            LocalBroadcastManager.getInstance(this).registerReceiver(receiver, new IntentFilter(LocationTransmitterService.NOTIFICATION_LOCATION));
+        }
     }
 
     @Override
     protected void onStop() {
+        if (receiver != null) {
+            LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
+        }
         super.onStop();
     }
 
@@ -148,6 +181,10 @@ public class ShowRouteActivity extends Activity {
         }
         outState.putInt(MEMBER_ROUTE_FIELD_FIRST, fieldFirst);
         outState.putInt(MEMBER_ROUTE_FIELD_LAST, fieldLast);
+        if (eventId > 0) {
+            outState.putLong(MEMBER_EVENT_ID, eventId);
+            outState.putParcelableArrayList(MEMBER_ROUTE_TRACK, new ArrayList<PolylineOptions>(routeTrack));
+        }
 
         super.onSaveInstanceState(outState);
     }
@@ -214,10 +251,79 @@ public class ShowRouteActivity extends Activity {
         routeHighlightLine = googleMap.addPolyline(routeHighlight);
     }
 
+    private void drawTrack() {
+        // Linien entfernen
+        for (Polyline subline : routeTrackLines) {
+            subline.remove();
+        }
+        routeTrackLines.clear();
+
+        for (PolylineOptions opt : routeTrack) {
+            routeTrackLines.add(googleMap.addPolyline(opt));
+        }
+    }
+
+    private int colorForSpeed(float speed) {
+        if (speed <= 0.0f) {
+            return Color.DKGRAY;
+        }
+        else if (speed <= 5.0f) {
+            return Color.RED;
+        }
+        else if (speed <= 10.0f) {
+            return Color.GREEN;
+        }
+        else {
+            return Color.CYAN;
+        }
+    }
+
     private class LocationReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Toast.makeText(getApplicationContext(), "Receive", Toast.LENGTH_SHORT).show();
+
+
+            routeTrack.clear();
+            int[] visited = intent.getIntArrayExtra(LocationTransmitterService.NOTIFICATION_EXTRA_PASSED_WAYPOINTS);
+            long[] timestamps = intent.getLongArrayExtra(LocationTransmitterService.NOTIFICATION_EXTRA_PASSED_WAYPOINT_TIME);
+            Toast.makeText(getApplicationContext(), visited.length + " - " + timestamps.length, Toast.LENGTH_SHORT).show();
+
+            if (visited.length > 0 && visited.length == timestamps.length) {
+                List<LatLng> waypoints = route.getPoints();
+
+                PolylineOptions opt = new PolylineOptions();
+                opt.add(waypoints.get(visited[0]));
+                Float prevSpeed = null;
+
+                for (int i = 1; i < visited.length; i++) {
+                    long elapsedMs = timestamps[i]-timestamps[i-1];
+
+                    LatLng curWaypoint = waypoints.get(i);
+                    LatLng prevWaypoint = waypoints.get(i-1);
+
+                    float[] output = new float[1];
+                    Location.distanceBetween(curWaypoint.latitude, curWaypoint.longitude, prevWaypoint.latitude, prevWaypoint.longitude, output);
+                    float distanceM = output[0];
+
+                    float elapsedH = elapsedMs*3e6f;
+                    float distanceKm = distanceM/1000.0f;
+
+                    float speed = distanceKm/elapsedH;
+
+                    opt.add(waypoints.get(visited[i]));
+
+                    int color = colorForSpeed(speed);
+                    if ((prevSpeed != null && color != colorForSpeed(prevSpeed)) || i == visited.length-1) {
+                        opt.color(color);
+                        routeTrack.add(opt);
+
+                        opt = new PolylineOptions();
+                        opt.add(waypoints.get(visited[i]));
+                    }
+                    prevSpeed = speed;
+                }
+            }
+            drawTrack();
         }
     }
 }
