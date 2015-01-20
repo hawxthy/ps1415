9978194a91d95db6345266c5d8343b3101e1578e
RichardSchulze <richard-schulze@online.de>
2015-01-20 15:05:39 +0100

    Release

--- SkatenightBackend/src/main/java/ws1415/SkatenightBackend/Constants.java
+++ SkatenightBackend/src/main/java/ws1415/SkatenightBackend/Constants.java
@@ -7,19 +7,19 @@ public class Constants {
     /**
      * Die Basisadresse des Backends.
      */
-    public static final String BASE_URL = "https://skatenight-ms-testing.appspot.com";
+    public static final String BASE_URL = "https://skatenight-ms.appspot.com";
     /**
      * Client-ID für die normale App.
      */
-    public static final String ANDROID_USER_CLIENT_ID = "644721617929-mk0do3jm5lec1dasijdj3220ot87gbn7.apps.googleusercontent.com";
+    public static final String ANDROID_USER_CLIENT_ID = "37947570052-g006o3ovfotnjqreltom6c7cbktm7dap.apps.googleusercontent.com";
     /**
      * Client-ID für die Veranstalter-App.
      */
-    public static final String ANDROID_HOST_CLIENT_ID = "644721617929-kbcha00vb3j30sh9at05sagm73iltqqo.apps.googleusercontent.com";
+    public static final String ANDROID_HOST_CLIENT_ID = "37947570052-4ru7asfhnrjmmqvj3qdpo2rrp31fudf3.apps.googleusercontent.com";
     /**
      * Client-ID für Webaufrufe.
      */
-    public static final String WEB_CLIENT_ID = "644721617929-unb9em0kl73b9evdv2h52ufn26fao20p.apps.googleusercontent.com";
+    public static final String WEB_CLIENT_ID = "37947570052-dk3rjhgran1s38gscv6va2rmmv2bei8r.apps.googleusercontent.com";
 
     /**
      * Definiert die Webclient-ID als Android Audience, da aus den Apps der Zugriff auf das Back-
@@ -48,5 +48,5 @@ public class Constants {
     /**
      * Der Public API-Key, der in der Developer Console für GCM angelegt wurde.
      */
-    public static final String GCM_API_KEY = "AIzaSyCpwhxda1Lb5E61_fybZq2iSJgViZu3QNM";
+    public static final String GCM_API_KEY = "AIzaSyDO8mosWwYXjjGZ9besu9CZw1LDEEXrFXE";
 }
