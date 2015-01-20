d0e7afdd455122d07b63ffe5f02abb7a97ed5d9a
Bernd Eissing <b_eiss02@uni-muenster.de>
2015-01-20 12:44:50 +0100

    Funktionen umgelagert

--- SkatenightBackend/src/main/java/ws1415/SkatenightBackend/FieldType.java
+++ SkatenightBackend/src/main/java/ws1415/SkatenightBackend/FieldType.java
@@ -1,5 +1,8 @@
 package ws1415.SkatenightBackend;
 
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Created by Bernd Eissing on 05.12.2014.
  */
@@ -24,5 +27,53 @@ public enum FieldType {
 
     public int getId() {
         return id;
+    }
+
+    // ---------- Hilfsmethoden für den Umgang mit den dynamischen Eigenschaften ---------
+
+    /**
+     * Methode zum finden von Id's von eindeutigen Feldern.
+     *
+     * @param type der Typ des eindeutigen Feldes
+     * @return Die Id, -1 falls keins gefunden wurde
+     */
+    public static int getUniqueFieldId(FieldType type, Event e){
+        for(int i = 0; i < e.getDynamicFields().size(); i++){
+            if(e.getDynamicFields().get(i).getType() == type.getId()){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Verbindet die beiden Datumsangaben des Events zu einem.
+     * @return Das zusammengefügte Date.
+     */
+    public static Date getFusedDate(Event e) {
+        Date time = new Date(Long.parseLong(getUniqueField(FieldType.TIME.getId(), e).getValue()));
+        Date date = new Date(Long.parseLong(getUniqueField(FieldType.DATE.getId(), e).getValue()));
+        Calendar cal1 = Calendar.getInstance();
+        cal1.setTime(date);
+        Calendar cal2 = Calendar.getInstance();
+        cal2.setTime(time);
+        cal1.set(Calendar.HOUR_OF_DAY, cal2.get(Calendar.HOUR_OF_DAY));
+        cal1.set(Calendar.MINUTE, cal2.get(Calendar.MINUTE));
+        return cal1.getTime();
+    }
+
+    /**
+     * Methode zum finden von eindeutigen Feldern
+     *
+     * @param type der Typ des eindeutigen Feldes
+     * @return Das Feld, null falls keins gefunden wurde
+     */
+    public static Field getUniqueField(int type, Event e) {
+        for (int i = 0; i < e.getDynamicFields().size(); i++) {
+            if (e.getDynamicFields().get(i).getType() == type) {
+                return e.getDynamicFields().get(i);
+            }
+        }
+        return null;
     }
 }
