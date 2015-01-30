b66e52bdc8f0611e0c2ea70ea8f7e4721933f779
m_wrod01 <m_wrod01@uni-muenster.de>
2015-01-30 11:19:47 +0100

    - UserGroupAdapter erstellt

--- app/src/main/java/ws1415/ps1415/adapter/UsergroupAdapter.java
+++ app/src/main/java/ws1415/ps1415/adapter/UsergroupAdapter.java
@@ -1,7 +1,96 @@
 package ws1415.ps1415.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import ws1415.ps1415.R;
+
 /**
+ * Dieser Adapter wird für das Anzeigen der Usergruppen in der Liste genutzt.
+ *
  * Created by Martin on 30.01.2015.
  */
-public class UsergroupAdapter {
+public class UsergroupAdapter extends BaseAdapter {
+    private List<UserGroup> groupList = new ArrayList<UserGroup>();
+    private Context context;
+    private LayoutInflater inflater;
+
+    /**
+     * Konstruktor, der den Inhalt der Liste festlegt;
+     *
+     * @param context   Context, von dem aus der Adapter aufgerufen wird.
+     * @param groupList Liste von den Nutzergruppen
+     */
+    public UsergroupAdapter(Context context, List<UserGroup> groupList) {
+        this.context = context;
+        this.groupList = groupList;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    /**
+     * Gibt die Anzahl der Nutzergruppen zurück.
+     *
+     * @return Anzahl der Nutzergruppen
+     */
+    @Override
+    public int getCount() {
+        if (groupList == null) return 0;
+        else return groupList.size();
+    }
+
+    /**
+     * Gibt die Nutzergruppe an der Stelle i in der Liste zurück.
+     *
+     * @param i Stelle
+     * @return Nutzergruppe an Stelle i
+     */
+    @Override
+    public UserGroup getItem(int i) {
+        return groupList.get(i);
+    }
+
+    /**
+     * Gibt die ID der Nutzergruppe in der Liste zurück.
+     *
+     * @param i Stelle der Nutzergruppe
+     * @return ID der Nutzergruppe
+     */
+    public long getItemId(int i) {
+        return i;
+    }
+
+    /**
+     * Klasse zum Halten der GUI Element, damit keine Kopien erstellt werden.
+     */
+    private class Holder {
+        private TextView groupName;
+        private TextView groupCreator;
+        private TextView groupCount;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        Holder holder;
+
+        if (convertView == null) {
+            holder = new Holder();
+            convertView = inflater.inflate(R.layout.list_view_item_usergroup, viewGroup, false);
+            holder.groupName = (TextView) convertView.findViewById(R.id.list_view_item_usergroup_name);
+            holder.groupCreator = (TextView) convertView.findViewById(R.id.list_view_item_usergroup_creator);
+            holder.groupCount = (TextView) convertView.findViewById(R.id.list_view_item_usergroup_member);
+            convertView.setTag(holder);
+        } else {
+            holder = (Holder) convertView.getTag();
+        }
+
+        holder.groupName.setText(getItem(position).getName());
+        holder.groupCreator.setText(getItem(position).getCreator());
+        holder.groupCount.setText(getItem(position).getMember().size());
+
+        return convertView;
+    }
 }
