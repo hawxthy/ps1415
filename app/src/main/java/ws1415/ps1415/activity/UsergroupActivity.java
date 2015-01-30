43f54e8a11b0fc9b996006c64590914d6727fd85
m_wrod01 <m_wrod01@uni-muenster.de>
2015-01-30 10:23:46 +0100

    - Nutzergruppen Branch erstellt

---
+++ app/src/main/java/ws1415/ps1415/activity/UsergroupActivity.java
@@ -0,0 +1,7 @@
+package ws1415.ps1415.activity;
+
+/**
+ * Created by Martin on 30.01.2015.
+ */
+public class UsergroupActivity {
+}
---
+++ app/src/main/java/ws1415/ps1415/adapter/UsergroupAdapter.java
@@ -0,0 +1,7 @@
+package ws1415.ps1415.adapter;
+
+/**
+ * Created by Martin on 30.01.2015.
+ */
+public class UsergroupAdapter {
+}
