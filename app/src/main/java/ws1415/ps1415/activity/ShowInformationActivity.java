59726ec4e3ad900282f9b5cac94185f58815d082
Pascal Otto <p_otto03@uni-muenster.de>
2015-02-24 17:10:57 +0100

    LocationTransmitterService wird bei laufender Aktueller Auswertung gestartet.

--- app/src/main/java/ws1415/ps1415/activity/ShowInformationActivity.java
+++ app/src/main/java/ws1415/ps1415/activity/ShowInformationActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
